9dca85c6d9a1ace70418aa922180f81b04e64b4a
steffiehlee <144110105+steffiehlee@users.noreply.github.com>
2023-11-10 11:09:42 +0800

    updated dashboard

--- revenue_tracker.py
+++ revenue_tracker.py
@@ -40,11 +40,11 @@ df1 = convert_excel_bytes_to_dataframe(
         "Overall"
     )
 
-instal = df[['Student Name', 'Batch', 'Course', '# of Installments', 'Installment price',
+instal = df[['Student Name', 'Batch', 'Course', '# of Installments', 'Paid Installment', 'Installment price',
              'First payment date', 'Payment Status']]
 instal[['Course Price', 'Fees Owed']] = df[['Course Price', 'Fees Owed']].astype(float)
 instal = instal[instal.Batch.notnull()]
-instal.dropna(subset=['# of Installments'], inplace=True)
+instal.dropna(subset=['# of Installments', 'First payment date'], inplace=True)
 instal['# of Installments'] = instal['# of Installments'].astype(int)
 instal = instal.reset_index()
 instal = instal.drop('index', axis=1)
@@ -57,7 +57,7 @@ time_range = []
 #get current month
 
 cmonth = datetime.now()
-for i in range(7):
+for i in range(6):
     cmonths = cmonth+relativedelta(months=i+1)
     time_range.append(cmonths.replace(day=1, hour=0,minute=0,second=0,microsecond=0))
 
@@ -65,11 +65,11 @@ student = instal['Student Name']
 instal_amt = instal['Installment price']
 stpaydate = []
 
+#Get the expected payday of all students
 for i in range(len(instal)):
     payday = []
     first = instal['First payment date'][i]
     pay_count = instal['# of Installments'][i]
-
     for j in range(pay_count):
         pay = first+relativedelta(months=j)
         payday.append(pay)
@@ -77,9 +77,10 @@ for i in range(len(instal)):
 
 instal['Payday'] = stpaydate
 
-e_pmt = list(repeat(0, 7))
 
-for i in range(7):
+e_pmt = list(repeat(0, len(time_range)))
+
+for i in range(len(time_range)):
     month=time_range[i]
     for j in range(len(stpaydate)):
         for k in stpaydate[j]:
@@ -87,7 +88,7 @@ for i in range(7):
                 e_pmt[i]=e_pmt[i]+instal_amt[j]
 
 
-for i in range(7):
+for i in range(len(time_range)):
     time_range[i]=time_range[i].strftime("%Y-%m")
 
 exp = pd.DataFrame({
@@ -95,63 +96,91 @@ exp = pd.DataFrame({
     "Expected Installment": e_pmt
 })
 
-installment_graph = px.bar(exp, x="Month", y="Expected Installment", title='Installment Payments in Future')
+
+
+
+#installment_graph = px.bar(instal, x="Month", y="Expected Installment", title='Installment Payments in Future')
+
 rev = df1[['Course', 'Cohort Start Date', 'Revenue\n(Formulated)']]
 rev = rev.dropna(subset=['Course'])
 rev['Cohort Start Date'] = df1['Cohort Start Date'].astype(str)
 rev = rev.reset_index()
 rev = rev.drop('index', axis=1)
-revenue_graph = px.bar(rev, x="Cohort Start Date", y="Revenue\n(Formulated)", color="Course", barmode="stack",
-                       title="Revenue by course")
-revenue_graph.update_xaxes(type="category")
+
 
 df1.dropna(subset='Cohort Start Date', inplace=True)
-
 hc = df1[['Course', 'Cohort Start Date', 'Student Count']]
 hc['Cohort Start Date'] = df1['Cohort Start Date'].astype(str)
-
 drop_lst = ['All']
 cohort_start = hc['Cohort Start Date'].unique()
 for c in cohort_start:
     drop_lst.insert(1, c)
 
-#status = instal['Payment Status']
+#Track all students who have not finished payments
 track = instal[instal['Payment Status'].isin(['Still Paying', 'Overdue Payment'])]
 track.dropna(subset='Fees Owed', inplace=True)
 track['Received'] = list(map(lambda x, y: x-y, track['Course Price'], track['Fees Owed']))
-#track['Paid Installment'] = list(map(lambda x, y: x//y, track['Course Price'], track['Received']))
-#track['Paid Installment'] = track['Paid Installment'].astype(int)
 
-exp_cmonth = []
-rec_cmonth = []
+track['Month Received'] = [0]*len(track)
+track['Pay Date'] = ['na']*len(track)
 exp_todate = []
-tcond = lambda x: x.date() < cmonth.date()
+
+tcond = lambda x: x.date() <= cmonth.date()
+same_month = lambda x, y: x.month == y.month and x.year == y.year
 for s in track['Student Name']:
     row = track.loc[track['Student Name'] == s]
+    payd = row['Payday'].values[0]
     if row['Payment Status'].values[0] == 'Overdue Payment':
         ep = row['Course Price'].values[0]
+        track.loc[track['Student Name'] == s, 'Pay Date'] = payd[-1]
     else:
-        payd = row['Payday'].values[0]
         count = sum(1 for t in payd if tcond(t))
-        ep = row['Installment price'].values[0]*count
+        ip = row['Installment price'].values[0]
+        ep = ip*count
+        for index, date in enumerate(payd):
+            if same_month(date, cmonth):
+                track.loc[track['Student Name'] == s, 'Pay Date'] = date
+                if tcond(date):
+                    if (index+1) == row['Paid Installment'].values[0]:
+                        track.loc[track['Student Name'] == s, 'Month Received'] = ip
+
     exp_todate.append(ep)
 
 track['Expected'] = exp_todate
+track[['Month Received', 'Installment price']] = track[['Month Received', 'Installment price']].astype(float)
 
+sub = track[track['Payment Status'].isin(['Still Paying'])]
+
+#Plot tracking graphs
+revenue_graph = px.bar(rev, x="Cohort Start Date", y="Revenue\n(Formulated)", hover_name="Course", barmode="stack",
+                       title="Revenue by course")
+revenue_graph.update_xaxes(type="category")
+
+installment_graph = px.bar(exp, x="Month", y="Expected Installment", title='Installment Payments in Future')
+
+#Tracking all payment to date by student
 pmt_todate = px.bar(
     track, x="Student Name", y="Received",
-    color='Payment Status', #color_discrete_sequence=px.colors.qualitative.Set1,
+    color='Payment Status',
     color_discrete_map={'Overdue Payment': 'red', 'Still Paying': '#3366CC'},
-    title="Expected Payment vs Received Payment(to date)"
+    title="Received Payment(to date)"
 )
 pmt_todate.add_trace(
     go.Scatter(x=track['Student Name'], y=track['Expected'], name="Expected",
-               mode="lines")
+               #color=track['Payment Status'],
+               mode="lines+markers",
+               )
 )
-#fig = px.line(track, x='Student Name', y='Expected')
-#fig.add_bar(x=track['Student Name'], y=track['Received'], name='Received', color=track['Payment Status'])
-#fig.update_layout(barmode='group')
+pmt_todate.update_traces(xaxis='x2')
 
+track_month = px.bar(
+    sub, x="Pay Date", y=["Month Received", "Installment price"],
+    color_discrete_sequence=px.colors.qualitative.Set2, hover_name='Student Name',
+    barmode='group', title=f'Installment Tracker for {cmonth.year}-{cmonth.month}'
+)
+track_month.update_layout(
+    yaxis_title="Amount ($)",
+)
 
 app.layout = html.Div(children=[
     html.H1(
@@ -163,13 +192,14 @@ app.layout = html.Div(children=[
 
     html.Div(children=[
         dcc.Graph(
-            id='fig1', figure=pmt_todate, style={'width': '50%', 'display': 'inline-block'}
+            id='fig1_1', figure=pmt_todate, style={'width': '50%', 'display': 'inline-block'}
         ),
         dcc.Graph(
-            id='fig2', figure=installment_graph, style={'width': '50%', 'display': 'inline-block'}
+            id='fig1_2', figure=installment_graph, style={'width': '50%', 'display': 'inline-block'}
         ),
     ]),
 
+    dcc.Graph(id='fig2_1', figure=track_month, style={'width': '33%', 'display': 'inline-block'}),
 
 
     html.Div(children=[
--- sales_dashboard/test.py
+++ sales_dashboard/test.py
@@ -1,63 +1,19 @@
-from dateutil.relativedelta import relativedelta
-import plotly.express as px
-import calendar
-from datetime import datetime, timedelta
+import itertools
 import pandas as pd
-
-trange = []
-cmonth = datetime(2023, 4, 1)
-for i in range(7):
-    cmonths = cmonth+relativedelta(months=i+1)
-    trange.append(cmonths)
-# Define the starting date
-
-now = datetime.now()
-
-for c in trange:
-    if c.month == now.month and c.day < now.day:
-        print('True')
-print(cmonth.date)
-
-print(now)
-
-# Sample list
-my_list = [10, 20, 30, 40, 50]
-print(sum(my_list))
-# Define the condition
-"""
-condition = lambda x: x > 30
-
-# Find the first element that matches the condition and its location
-for index, element in enumerate(my_list):
-    if condition(element):
-        print(f"Found element {element} at index {index}")
-        break
-else:
-    print("No element matching the condition found")
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
-# Count the number of elements that satisfy the condition
-count = sum(1 for element in my_list if condition(element))
+# Two sample lists
+list1 = [1, 2]
+list2 = ['a', 'b', 'c']
 
-# Print the count
-print(count)
+# Perform Cartesian product
+cartesian_product = list(itertools.product(list1, list2))
 
-
-# Get the current date
-current_date = datetime.now()
-
-# Get the number of days in the current month
-_, num_days = calendar.monthrange(current_date.year, current_date.month)
-
-# Generate a list of all dates in the current month
-dates_in_month = [current_date.replace(day=day) for day in range(1, num_days + 1)]
-
-# Print the dates
-for date in dates_in_month:
-    print(date)
-
-month=datetime.now()
-
-for i in range(7):
-    print(month-relativedelta(months=i))
-"""
+# Print the Cartesian product
+df = pd.DataFrame({
+    'Name': ['John', 'Alice', 'Bob', 'Emily'],
+    'Age': [25, 30, 35, 28],
+    'City': ['New York', 'London', 'Paris', 'Sydney']
+})
